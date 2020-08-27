edc20494b50f08f64618d0aa1d6883c0800b2cc1
左浩 <hao.zuo2@zhenai.com>
2020-08-27 19:32:23 +0800

    Initial commit

---
+++ javabase/src/main/java/com/meng/daily/javabase/JavabaseApplication.java
@@ -0,0 +1,12 @@
+package com.meng.daily.javabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavabaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavabaseApplication.class, args);
+    }
+}
---
+++ javabase/src/main/java/com/meng/daily/javabase/service/HelloService.java
@@ -0,0 +1,19 @@
+package com.meng.daily.javabase.service;
+
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class HelloService {
+
+    public static void main(String[] args) {
+
+    }
+
+
+    public static String fun(int num) {
+        return String.valueOf(num + 1000);
+    }
+}
