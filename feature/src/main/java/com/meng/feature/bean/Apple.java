d6e8ba52835f43a089f1770387e090ae334adf6e
左浩 <hao.zuo2@zhenai.com>
2020-09-03 11:18:00 +0800

    feature

---
+++ feature/src/main/java/com/meng/feature/bean/Apple.java
@@ -0,0 +1,28 @@
+package com.meng.feature.bean;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @Description 苹果类
+ * @Author ZuoHao
+ * @Date 2020/9/2 20:28
+ */
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+public class Apple {
+    private int weight = 0;
+    private String color = "";
+
+
+
+
+    public static boolean isGreen(Apple apple) {
+        return "green".equals(apple.getColor());
+    }
+}
