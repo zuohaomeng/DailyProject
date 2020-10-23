b9479f4d3224edbb8a8fe2847294e94972495fd5
左浩 <hao.zuo2@zhenai.com>
2020-10-23 14:46:40 +0800

    Lambda

---
+++ javabase/src/main/java/com/meng/daily/javabase/test/Console.java
@@ -0,0 +1,16 @@
+package com.meng.daily.javabase.test;
+
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.Arrays;
+
+/**
+ * @Description
+ * @Author ZuoHao
+ * @Date 2020/10/23 10:19
+ */
+public class Console {
+    public static void main(String[] args) {
+
+    }
+}
