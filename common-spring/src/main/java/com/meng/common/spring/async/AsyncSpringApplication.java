688ffe0c05fb39bb2a37dc22f6555f1895988b67
zuohao <178183852@qq.com>
2021-03-13 17:29:17 +0800

    日常

---
+++ common-spring/src/main/java/com/meng/common/spring/async/AsyncSpringApplication.java
@@ -0,0 +1,16 @@
+package com.meng.common.spring.async;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author ZuoHao
+ * @date 2021/3/13
+ */
+@SpringBootApplication
+public class AsyncSpringApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AsyncSpringApplication.class, args);
+    }
+
+}
